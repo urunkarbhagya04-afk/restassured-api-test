4c3c4ebe559928c302b90ef23d081c27513f1cd4
urunkarbhagya04-afk <urunkarbhagya04@gmail.com>
2025-11-01 22:07:14 +0000

    Initial REST Assured API test project

---
+++ src/test/java/com/api/testing/AppTest.java
@@ -0,0 +1,14 @@
+package com.api.testing;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testExample() {
+        // ✅ Simple sample test
+        assertTrue(true, "This test should always pass!");
+    }
+}
+
